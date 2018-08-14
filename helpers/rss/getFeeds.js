fa175186a6ff9808f4cfc58fdfc49d35e3551e78
Daniel Goldshlack <daniel@goldshlack.net>
2018-08-13 23:10:13 -0400

    Do not track `feeds.json` in git

--- helpers/rss/getFeeds.js
+++ helpers/rss/getFeeds.js
@@ -1,6 +1,21 @@
+const { existsSync } = require('fs');
+const { resolve } = require('path');
+
+const feedsPath = '../../feeds.json';
+const feedsFilepath = resolve(__dirname, feedsPath);
+const feedsPresent = existsSync(feedsFilepath);
+
+if (!feedsPresent) {
+  console.log('Create your personal `feeds.json`, please!');
+}
+
+const FEEDS = feedsPresent
+  ? require(feedsPath)
+  : { feeds: [] };
+
 const {
   feeds = [],
-} = require('../../feeds.json');
+} = FEEDS;
 
 const getFeeds = () => {
   return feeds;
